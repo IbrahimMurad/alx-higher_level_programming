711b070dee2ce69abd24f2b2993350afef307091
IbrahimMurad <ibrahimmorad31@gmail.com>
2023-09-08 01:22:13 +0300

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     import sys
+
     ac = len(sys.argv)
     if not ac == 4:
-        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     match sys.argv[2]:
         case "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
             sys.exit(0)
         case "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
             sys.exit(0)
         case "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
             sys.exit(0)
         case "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
             sys.exit(0)
         case _:
             print("Unknown operator. Available operators: +, -, * and /")
