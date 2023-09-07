68dce730a2f72d7079e3d34d28df2df13e0bf41c
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-09-07 22:07:04 +0300

    listing names of a module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    import sys
-    names = dir('hidden_4')
+    names = dir(hidden_4)
     for name in names:
         if not name.startswith("__"):
             print("{}".format(name))
