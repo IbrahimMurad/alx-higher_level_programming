3a548a4d957e1ad879bdcff6d9c45054a417b78b
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-09-13 14:02:21 +0300

    max value index

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if type(a_dictionary) is not dict:
+        return "None"
+    else:
+        keys = list(a_dictionary.keys())
+        values = list(a_dictionary.values())
+        max_value = max(values)
+        max_value_index = values.index(max_value)
+        key_of_max_value = keys[max_value_index]
+        return key_of_max_value
