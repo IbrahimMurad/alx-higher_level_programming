41a7470e31e916c3d6f51580a7e34d66bc3e3220
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-09-26 20:36:47 +0300

    6-square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+""" This module focuses on building a class with private instance attribute
+ with certain condtions, raising esceptions if not satisfied. """
+
+
+class Square:
+    """ This class holds a size value. """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ This initializes the size value. """
+
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """ Returns the area of the size. """
+        return self.size * self.size
+
+    def my_print(self):
+        """ prints in stdout the square with the character #. """
+        if self.size == 0:
+            print()
+        else:
+            for y in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                for x in range(self.position[0]):
+                    print(" ", end='')
+                for j in range(self.size):
+                    print("#", end='')
+                print()
+
+    @property
+    def size(self):
+        """ Returns the value 'size'. """
+        return self.__size
+
+    @property
+    def position(self):
+        """ Returns the value 'size'. """
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """ sets size to value. """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """ sets size to value. """
+
+        if type(value) is not tuple and not len(value) == 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
