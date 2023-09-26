ce4fb366d90d69bfb9ff97ffa619033e8323aef5
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-09-26 19:44:54 +0300

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" This module focuses on building a class with private instance attribute
+ with certain condtions, raising esceptions if not satisfied. """
+
+
+class Square:
+    """ This class holds a size value. """
+
+    def __init__(self, size=0):
+        """ This initializes the size value. """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Returns the area of the size. """
+        return self.__size * self.__size
