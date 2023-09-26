e38517a14ce8c247b5595df3383bf5448a7faa74
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-09-27 01:40:08 +0300

    magic_calculation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,18 +4,18 @@
 the radius of a circle, and calculates its area and its circumference """
 math = __import__('math')
 
+
 class MagicClass:
     """ This class gets the radius and
     calculates the area and the circumference. """
 
     def __init__(self, radius=0):
-        """ Initializes the value of teh radius. """
+        """ Initializes the value of the radius. """
 
         self.__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        else:
-            self.__radius = radius
+        self.__radius = radius
 
     def area(self):
         """ Calculates the area. """
