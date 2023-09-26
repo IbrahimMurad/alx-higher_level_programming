5eb93dd480044d8a83292868bf8c98dc7579f8af
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-09-26 18:00:07 +0300

    mt first class passes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" This is a demonistration of how to make a class. """
+class Square:
+    """ This class do nothing """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+Now, it is the time for OOP in Python
