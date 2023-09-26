aaf194b15f2c6e697db74c484c9fb3f81c980c82
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-09-27 01:34:29 +0300

    magic_calculation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,7 +2,7 @@
 
 """ This module defines a class names 'MagicClass' that holds
 the radius of a circle, and calculates its area and its circumference """
-
+math = __import__('math')
 
 class MagicClass:
     """ This class gets the radius and
@@ -20,9 +20,9 @@ class MagicClass:
     def area(self):
         """ Calculates the area. """
 
-        return __import__('math').pi.pi * (self.__radius ** 2)
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
         """ Calculates the circumference."""
 
-        return 2 * __import__('math').pi * self.__radius
+        return 2 * math.pi * self.__radius
