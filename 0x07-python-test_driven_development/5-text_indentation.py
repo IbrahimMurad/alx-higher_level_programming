a77625e22b29951efc5b9cdf4ccc3f94cde3b3fd
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-10-01 20:18:51 +0300

    text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+"""
+This module defines a function that takes a string as an input
+and prints it with 2 new lines after each of 
+these characters: ., ? and :
+<text> must be a string otherwise raise TypeError
+There should be no space at the beginning or at the end of each printed line
+"""
+
+
+def text_indentation(text):
+    """
+    prints <text> with 2 newlines after ., ?, :
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    temp_str = ""
+    list_of_lines = []
+    for c in text:
+        if c not in ['.', ':', '?']:
+            temp_str += c
+        else:
+            list_of_lines.append(temp_str.strip())
+            list_of_lines[-1] += '\n'
+            temp_str = ""
+    if not len(temp_str) == 0:
+        list_of_lines.append(temp_str.strip())
+    for s in list_of_lines:
+        print("{}".format(s))
