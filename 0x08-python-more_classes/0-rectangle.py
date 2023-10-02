68b51add615c570063f5f2630b7e62ea5828a87c
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-10-02 18:14:12 +0300

    empty rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module defines a class called rectangle
+This class is empty and do nothing
+"""
+
+
+class Rectangle:
+    """ This class do nothing
+    """
+
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Classes level 2
