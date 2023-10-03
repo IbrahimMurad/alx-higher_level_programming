476053e7de511fa3d19b94cb69a4c1742cd21750
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-10-03 21:43:16 +0300

    slots

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,12 @@
+#!/usr/bin/python3
+""" This module defines a class that has no attributes
+and prevents any creation of attributes except for 'first_name'
+"""
+
+
 class LockedClass:
+    """ a class that has only one job, to prevent creating any extra
+    attributes but 'first_name'
+    """
+
     __slots__ = ['first_name']
