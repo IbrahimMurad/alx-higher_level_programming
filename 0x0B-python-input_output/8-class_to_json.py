e7b14f40767b9d1b4fae7ba9ed4f100cf1d70055
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-10-10 21:45:39 +0300

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,6 +3,8 @@
 returns the dictionary description with simple data structure
 (list, dictionary, string, integer and boolean)
 for JSON serialization of an object """
+
+
 loads = __import__("json").loads
 dumps = __import__("json").dumps
 
