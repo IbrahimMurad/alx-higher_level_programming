91b1d8bd067b2d3aa9ceb427a507c42e9936c501
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-11-01 02:06:18 +0200

    Revert "Delete 0x07-python-test_driven_development directory"
    This reverts commit d8e5b65c2866ce1163945cb507a5c599c08ed322.

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module defines a function that adds two integer
+the two integers must be integers or floats
+otherwise through TypeError exception
+"""
+
+
+def add_integer(a, b=98):
+    """
+    adds two integers
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module defines a function that prints a square with the character #
+<size> is the side length of the square
+<size> must be an integer or raise TypeError
+if <size> is less than 0, raise a ValueError
+if <size> is a float and is less than 0, raise a TypeError
+"""
+
+
+def print_square(size):
+    """
+    prints a square with #
+    """
+
+    if type(size) not in [int, float]:
+        raise TypeError("size must be an integer")
+    if type(size) is int and size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0.0:
+        raise TypeError("size must be an integer")
+    for i in range(int(size)):
+        print("{}".format('#' * int(size)))
