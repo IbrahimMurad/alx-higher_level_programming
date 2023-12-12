dc39b2b1ad5893eda24d9969d009d0dc90bfddee
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-12-12 17:41:55 +0200

    show table creation

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,4 @@
+-- Active: 1702391851621@@127.0.0.1@3306@hbtn_0c_0
 -- creates a table called first_table in the current database in your MySQL server.
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
