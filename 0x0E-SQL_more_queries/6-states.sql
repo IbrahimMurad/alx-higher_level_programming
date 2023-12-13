669918e55813aca93da395dfca9220443458559b
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-12-13 19:35:29 +0200

    foreign key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
+-- Active: 1702391851621@@127.0.0.1@3306@hbtn_0d_usa
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
