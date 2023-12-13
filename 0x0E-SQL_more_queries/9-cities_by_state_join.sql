e32beb338a8ba51ef23d308f57239b31cd16fb22
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-12-13 20:22:55 +0200

    state - city

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, states.name, cities.name FROM cities, states WHERE cities.state_id = states.id;
+SELECT cities.id, states.name, cities.name FROM cities LEFT JOIN states ON cities.state_id = states.id;
