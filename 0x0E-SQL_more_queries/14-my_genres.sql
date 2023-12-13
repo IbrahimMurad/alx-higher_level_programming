18cfb17ce111a3c5b5043c525d59fb44e5373459
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-12-13 21:59:53 +0200

    Dexter genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,5 @@ FROM tv_shows
 	ON tv_shows.id = tv_show_genres.show_id
 	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 	
-	WHERE tv_shows.title = 'Dexter';
+	WHERE tv_shows.title = 'Dexter'
+	ORDER BY tv_genres.name ASC;
