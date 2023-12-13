d222697eb7566b3418f46c70eda19d1b0595fcd5
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-12-13 14:40:21 +0200

    show grants

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Active: 1702391851621@@127.0.0.1@3306@hbtn_0c_0
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on my server (in localhost).
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
