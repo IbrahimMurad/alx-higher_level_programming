519676031c9d03a8d0b443ea39b75ebdca3eb80e
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-12-13 04:28:59 +0200

    CHAR SET AND COLLATE

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
-ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci CHARACTER SET utf8mb4;
+ALTER DATABASE hbtn_0c_0 DEFAULT COLLATE utf8mb4_unicode_ci CHARACTER SET utf8mb4;
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
