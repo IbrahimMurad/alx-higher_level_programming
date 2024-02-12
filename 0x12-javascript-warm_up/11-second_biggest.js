e99ab6e8343e3f8d144b7f2cf1d6f8f92830c83e
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-12 18:41:38 +0200

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,8 @@ const myList = process.argv.slice(2).sort();
 const listLength = myList.length;
 if (listLength === 0) {
   console.log(0);
+} else if (listLength === 1) {
+  console.log(myList[0]);
 } else {
   console.log(myList[listLength - 2]);
 }
