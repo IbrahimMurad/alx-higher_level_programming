1d7afae4e16dcc763bd28eb4a8e7926b664fb9e1
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-13 07:32:27 +0200

    reactangles and squares

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        let line = '';
+        for (let j = 0; j < this.width; j++) {
+          line += c;
+        }
+        console.log(line);
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
