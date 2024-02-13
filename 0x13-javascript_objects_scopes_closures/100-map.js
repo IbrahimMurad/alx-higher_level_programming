5a194e46b014be2062f6c4125e408a480589194e
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-13 17:33:54 +0200

    100-map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 const oldList = require('./100-data').list;
-const newList = oldList.map((x) => x * oldList.indexOf(x));
+let i = 0;
+const newList = oldList.map((x) => x * i++);
 console.log(oldList);
 console.log(newList);
