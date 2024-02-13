73b6a01d23bdaafc34e721b0458f5aa5437bcc34
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-13 18:06:50 +0200

    101-sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,12 +5,10 @@ const listOfKeys = [];
 for (const key in dict) {
   listOfKeys.unshift(key);
 }
-console.log(listOfKeys);
 const listOfValues = [];
 for (const key of listOfKeys) {
   listOfValues.unshift(dict[key]);
 }
-console.log(listOfValues);
 const newDict = {};
 for (let i = 0; i < listOfKeys.length; i++) {
   newDict[listOfValues[i]] = [];
