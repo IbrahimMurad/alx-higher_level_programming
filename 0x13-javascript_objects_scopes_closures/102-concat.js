21195e3e9a2828a2f83c2f882b44620dcd04c754
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-14 04:50:50 +0200

    102-concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
 
-console.log(process.argv);
 const fs = require('fs');
 let file1Conent;
-fs.readFile('./filaA', 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data1) => {
 	if (err) {
 		console.log(err);
 		return;
 	}
-	console.log(data);
-	file1Conent = data;
+	fs.readFile(process.argv[3], 'utf-8', (err, data2) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		fs.writeFile(process.argv[4], data1 + data2, (err) => {
+			if (err) {
+				console.log(err);
+				return;
+			}
+		});
+	});
 });
-console.log(file1Conent);
