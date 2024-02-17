44f134240810d78ee2b75e9fd903911a426309d2
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-17 14:05:13 +0200

    only states starting with N

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -1,4 +1,10 @@
 USE hbtn_0e_0_usa;
 
-SELECT * FROM states WHERE states.name REGEXP '^N';
+CREATE TABLE IF NOT EXISTS states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+SELECT states.id , states.name FROM states WHERE states.name REGEXP '^N' ORDER BY states.id ASC;
 DROP TABLE states;
