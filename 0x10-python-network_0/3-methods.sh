e5bb906e56f1841828a64cee5a94ddd5bb66fefb
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-29 18:09:13 +0200

    allow

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
+# takes in a URL and displays all HTTP methods the server will accept
 curl -sI $1 | grep -i "Allow:" | cut -f 2- -d " "
