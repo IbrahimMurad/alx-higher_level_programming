ebc5da8b895e73654f3dcb3f3824eb4ca7373e3f
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-29 21:10:19 +0200

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+"""In this module, we define find_peak function
+that returns the peak inside a list"""
+
+
+def find_peak(my_list=list):
+    """ returns the peak value in my_list """
+
+    if len(my_list) == 0:
+        return None
+    else:
+        return sorted(my_list)[-1]
