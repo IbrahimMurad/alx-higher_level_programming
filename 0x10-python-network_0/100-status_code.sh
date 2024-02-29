4fcfc775f66a8754be1c8ef31a18c21f1c6d1e3c
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-29 22:11:53 +0200

    status_code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -f -w '%{response_code}' -o /dev/null $1
+curl -s -f -w '%{response_code}' $1
