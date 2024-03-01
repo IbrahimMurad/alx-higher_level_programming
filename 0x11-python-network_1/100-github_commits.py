dbdd30ee06fc851fd76acea1a11171f5e3d22d5d
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-01 20:39:34 +0200

    commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,11 +10,13 @@ import sys
 
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/repos/'
-    url += sys.argv[1] + '/' + sys.argv[2] + '/commits?per_page=10'
-    response = requests.get(url)
-    com_list = response.json()
-    for commit in com_list:
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[1], sys.argv[2]
+    )
+    para = {'per_page': 10}
+    response = requests.get(url, params=para)
+    commits = response.json()
+    for commit in commits:
         sha = commit.get('sha')
         author_name = commit.get('commit').get('author').get('name')
         print("{}: {}".format(sha, author_name))
